bf2dc129f65577e3166330b542da3e7597e11b62
P10Designs <designs.p10@gmail.com>
2020-09-11 13:16:35 +0200

    pre server

--- frontend/assets/js/widgets.js
+++ frontend/assets/js/widgets.js
@@ -8,16 +8,15 @@ document.body.onload = async () => {
 
 
 function matchWidget() {
-  const screenWidth = document.body.clientWidth;
-  const width = document.querySelectorAll('.match').length * 150;
-  const moveTimes = Math.trunc(width/screenWidth);
-  if (moveTimes < 1 || moveTimes == 0) {
-    document.querySelector('.right-arrow').style = "display:none";
-  }
-  document.querySelector('.left-arrow').addEventListener('click', () => matchMovement('left'));
-  document.querySelector('.left-arrow').style = "display: none";
-  document.querySelector('.right-arrow').addEventListener('click', () => matchMovement('right'));
-  document.querySelector('.settings').addEventListener('click', settings);
+  var url = 'http://localhost:5050/api/v1/matches'
+  fetch(url)
+  .then(response => 
+    response.json().then(res => ({
+        data: res,
+        status: response.status
+    })
+  ).then(res => {
+    console.log(res)}));   
   logger('info','Matches -> Loaded');
 }
 
@@ -35,40 +34,6 @@ function vodsWidget() {
 
 }
 
-function matchMovement(s){
-  const screenWidth = document.body.clientWidth;
-  const width = document.querySelectorAll('.match').length * 150;
-  const container = document.querySelector('.match-container');
-  const position = container.getBoundingClientRect().left;
-  
-  const moveTimes = Math.trunc(width/screenWidth);
-  const maxMove = -width*moveTimes
-
-  console.log(maxMove);
-  if(screenWidth < 721){
-    if (s == 'right') {
-      if (position > maxMove) {
-        container.scrollBy(150,0)
-      }
-      
-    }
-  } else if (screenWidth < 1075){
-    if (s == 'right') {
-      if (position > maxMove) {
-        container.style = `left: ${position-(150*2)}px`
-      }
-      
-    }
-  }else{
-
-  }
-
-  logger('debug', moveTimes);
-  logger('debug',screenWidth);
-  logger('debug', position);
-  logger('debug',width);
-}
-
 function settings(){
   console.log('settings');
-}
+} 
