6da907a14041e47f3582ea0b09b01bdfbcd9810f
P10Designs <designs.p10@gmail.com>
2020-09-22 20:16:02 +0200

    FInally match-widget is done 😃

--- frontend/assets/js/widgets.js
+++ frontend/assets/js/widgets.js
@@ -14,19 +14,67 @@ async function matchWidget() {
   if(json.length == 0){
     document.querySelector('.match-widget').style.display = "none";
   }else{
+    var before = "";
     document.querySelector('.match-container').innerHTML = "";
     for(var i = 0; i < json.length; i++){
-      const match = {
-        local: await teams(json[i].local.id),
-        visitor: await teams(json[i].visitor.id),
-        league: await leagues(json[i].league.id),
-        date: json[i].match_time,
+      if (json[i].played == false){
+        const match = {
+          local: await teams(json[i].local.id),
+          visitor: await teams(json[i].visitor.id),
+          league: await leagues(json[i].league.id),
+          date: await date(json[i].match_time),
+        }
+        if (before != match.date.weekday){
+          const matches = `<div class="match dayStarter">
+            <div class="day">
+                <span>${match.date.weekday.toUpperCase()}</span>
+              </div>
+            <div class="teams">
+              <div class="local">
+                <img src="${match.local.logo_url}" alt="LOCAL LOGO" class="team-logo">
+                <span class="aconimo">${match.local.acronym}</span>
+              </div>
+              <div class="visitor">
+                <img src="${match.visitor.logo_url}" alt="VISIT LOGO" class="team-logo">
+                <span class="aconimo">  ${match.visitor.acronym}</span>
+              </div>
+            </div>
+            <div class="league">
+              <span>${match.league.name.replace('Liga', '').toUpperCase()}</span>
+            </div>
+            <div class="time">
+              <span>${match.date.time}</span>
+            </div>
+          </div>`;
+          document.querySelector('.match-container').innerHTML +=  matches;
+          before = match.date.weekday
+        } else {
+          const matches = `<div class="match">
+            <div class="teams">
+              <div class="local">
+                <img src="${match.local.logo_url}" alt="LOCAL LOGO" class="team-logo">
+                <span class="aconimo">${match.local.acronym}</span>
+              </div>
+              <div class="visitor">
+                <img src="${match.visitor.logo_url}" alt="VISIT LOGO" class="team-logo">
+                <span class="aconimo">  ${match.visitor.acronym}</span>
+              </div>
+            </div>
+            <div class="league">
+              <span>${match.league.name.replace('Liga', '').toUpperCase()}</span>
+            </div>
+            <div class="time">
+              <span>${match.date.time}</span>
+            </div>
+          </div>`;
+          document.querySelector('.match-container').innerHTML +=  matches;
+        }
+      }else{
+        
       }
-      
     }
   }
   logger('info', 'Matches -> Loaded');
-  
 }
 function bannerWidget() {
   logger('info','Banner -> Loaded');
@@ -40,14 +88,7 @@ function vodsWidget() {
   logger('info','Vods -> Loaded');
 }
 
-function settings(){
-  console.log('settings');
-} 
 
-
-
-
-// LAST DONE: Create game constructor
 
 
 async function teams(id) {
@@ -70,3 +111,44 @@ async function leagues(id) {
   }
   return response;
 }
+
+async function date(date) {
+  var given = new Date(String(date), );
+  var today = new Date();
+
+  if((given.getUTCDate() - today.getUTCDate()) < 8){
+    var weekday = new Array();
+    weekday[0] = "Domingo";
+    weekday[1] = "Lunes";
+    weekday[2] = "Martes";
+    weekday[3] = "Miercoles";
+    weekday[4] = "Jueves";
+    weekday[5] = "Viernes";
+    weekday[6] = "Sabado";
+    var response = {
+      weekday: weekday[given.getUTCDay()],
+      time: `${given.getHours()}:${given.getMinutes()}`
+    }
+  }else{
+    var month = new Array();
+    month[0] = "Enero";
+    month[1] = "Febrero";
+    month[2] = "Marzo";
+    month[3] = "Abril";
+    month[4] = "Mayo";
+    month[5] = "Junio";
+    month[6] = "Julio";
+    month[7] = "Agosto";
+    month[8] = "Septiembre";
+    month[9] = "Octubre";
+    month[10] = "Noviembre";
+    month[11] = "Diciembre";
+    var response = {
+     weekday: `${given.getDate()} ${month[given.getMonth()]}`,
+     time: `${given.getHours()}:${given.getMinutes()}`
+    }
+  }
+  return response
+}
+
+//TODO: ALL WIDGETS EXCEPT MATCHES, NEED TO FINISH LEAGUES SELECTOR, NOT DONE YET, ITS DEACTIVATED ON HTML TOO 
